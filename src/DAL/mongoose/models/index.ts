1b017c7668f7935914b22433bca638e69106146e
justpsst <justpsst@yandex.ru>
2021-01-31 23:19:32 +0500

    fixed model typings

--- src/DAL/mongoose/models/index.ts
+++ src/DAL/mongoose/models/index.ts
@@ -1,4 +1,4 @@
 import * as mongoose from 'mongoose';
 import { Document } from 'mongoose';
 
-export const generateModel = <T extends Document>(modelName: string, schema: mongoose.Schema<Document<T>>) => mongoose.model<T>(modelName, schema);
+export const generateModel = <T extends Document>(modelName: string, schema: mongoose.Schema<Document & T>) => mongoose.model<T>(modelName, schema);
